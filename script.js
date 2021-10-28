c70414523482d5685526d9bbc423811ddba806e7
blocksan <sandyghosh555@gmail.com>
2021-10-28 16:47:03 +0530

    feat: docker build automated

--- script.js
+++ script.js
@@ -44,21 +44,7 @@ async function runDocker() {
     const git = simpleGit('./', { binary: 'git' });
 
     await git.add('./*')
-    await git.commit(`added game ${FOLDER_NAME}`)
+    await git.commit(`added game ${FOLDER_NAME} updated`)
     await git.push('origin', 'main');
   }
 runDocker();
-
-
-
-const options = {
-    baseDir: process.cwd(),
-    binary: 'git',
-    maxConcurrentProcesses: 6,
- };
- 
- // when setting all options in a single object
-//  const git = simpleGit(options);
- 
- // or split out the baseDir, supported for backward compatibility
- 
